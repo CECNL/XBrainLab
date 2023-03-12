89ec76e1ca5a6c06eb0418dcd7b8bcf24c5180be
yhcgg <yucgg127@gmail.com>
2023-03-13 00:00:03 +0800

    Add 3D saliency plot

--- XBrainLab/ui/visualization/__init__.py
+++ XBrainLab/ui/visualization/__init__.py
@@ -2,8 +2,9 @@ from .montage_picker import PickMontageWindow
 
 from .saliency_map import SaliencyMapWindow
 from .saliency_topomap import SaliencyTopographicMapWindow
+from .saliency_3Dplot import Saliency3DPlotWindow
 from .plot_abs_plot_figure import PlotABSFigureWindow
 from .plot_abs_topo_plot_figure import PlotTopoABSFigureWindow
 # from .model_summary import ModelSummaryWindow
 
-VISUALIZATION_MODULE_LIST = [SaliencyMapWindow, SaliencyTopographicMapWindow]
+VISUALIZATION_MODULE_LIST = [SaliencyMapWindow, SaliencyTopographicMapWindow, Saliency3DPlotWindow]
