9cd13e97c4778bf13206aa99fa463d432924047c
hchiaying <irishsieh0720.cs11@nycu.edu.tw>
2023-09-19 18:02:35 +0800

    nav bar optimization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,11 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
+
+
 extensions = [
 	# builtin
 	'sphinx.ext.autodoc',
@@ -38,18 +43,44 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
 autosummary_generate = True
 autodoc_default_options = {"inherited-members": None}
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
-
-html_sidebars = {
-    '**': ['globaltoc.html', 'searchbox.html'] 
+html_theme_options = {
+  # side bar setting
+  "navigation_with_keys": False,
+  "show_toc_level": 1,
+  "secondary_sidebar_items": ["page-toc", "edit-this-page"],
+  "article_header_start": [], # disable breadcrumbs
+  # nav bar External link icon
+  "icon_links_label": "External Links",
+  "icon_links": [
+        dict(
+            name="GitHub",
+            url="https://github.com/CECNL/XBrainLab",
+            icon="fa-brands fa-square-github",
+        ),],
+  # nav bar setting
+#   "logo": {
+# 	    "text":"XBrainLab",
+#         "alt_text": "Home",
+#     }
 }
+
+html_static_path = ['_static']
+html_logo = "_static/logo_title.svg"
+
+# left out "Created using Sphinx" in footnote
+html_show_sphinx = False 
+
+# special sidebar for mainpage
+html_sidebars = {
+	'index': ['globaltoc.html'],
+}
+
 
 bibtex_bibfiles = ["./references.bib"]
 bibtex_style = "unsrt"
